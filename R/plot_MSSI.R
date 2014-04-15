3a97ba81967c9623931451423a25369efc7a3716
Frank Pennekamp <pennekampster@gmail.com>
2014-04-15 15:50:42 +0200

    Improved plotting function for MSSI: specification of granularity to be plotted, labelled axis and title and updated documentation

--- R/plot_MSSI.R
+++ R/plot_MSSI.R
@@ -6,15 +6,16 @@
 #' and granulosities
 #' @param uniqueID Column name of the unique identifier for each trajectory in the dataframe
 #' @param time Column name containing the information on the time for each fix
+#' @param granulosity_choosen A numeric value of granulosity for plotting corresponding to the granulosity values choosen
 #' @param random Logical; whether the trajectories to be plotted are randomly sampled from the data 
 #' @param N_traj A numeric value specifying the number of trajectories to be plotted
 #' @return A plot showing the MSSI for each fix for different window sizes
-#' @param trajectory_select Vector containing the unique IDs of the trajectories to be plotted'
+#' @param trajectory_select Vector containing the unique IDs of the trajectories to be plotted
 #' @import ggplot2 
 #' @import gridExtra
 #' @export
 
-plot_MSSI <- function(raw_traj,data,uniqueID="traj",time="frame",random=T,N_traj=10,trajectory_select=select_traj){
+plot_MSSI <- function(raw_traj,data,uniqueID="traj",time="frame",granulosity_choosen=1,random=T,N_traj=10,trajectory_select=select_traj){
   
   #rename x and y variable to make function cap-insensitive
   colnames(raw_traj)[tolower(colnames(raw_traj)) == "x"] <- "x"
@@ -33,16 +34,17 @@ plot_MSSI <- function(raw_traj,data,uniqueID="traj",time="frame",random=T,N_traj
     traj <-  subset(data, uniqueID == select_traj[k])
     
     traj_MSSI <- ggplot(traj, aes(time, window_size)) +
-      geom_tile(data=traj, aes(width=as.numeric(granulosity),height=as.numeric(2),fill = SI)) + 
+      geom_tile(data=subset(traj, granulosity == granulosity_choosen), aes(width=as.numeric(granulosity),height=as.numeric(2),fill = SI)) + 
       scale_fill_gradientn(colours = c("red","cyan","black"), guide = "colourbar", limits=c(0,1))+
-      theme(legend.position="bottom")+
-      facet_wrap(~granulosity,ncol=4)
+      ylab("Window size")+
+      xlab("Time")+
+      theme(legend.position="bottom")
     
-    traj_plot <-  subset(raw_traj, uniqueID == select_traj[k])
-    gg_traj <- ggplot(traj_plot, aes(x,y,label=time))+ 
+     traj_plot <-  subset(raw_traj, uniqueID == select_traj[k])
+     gg_traj <- ggplot(traj_plot, aes(x,y,label=time))+ 
       geom_text(size=3)
     
-    print(grid.arrange(gg_traj,traj_MSSI))
+     print(grid.arrange(gg_traj,traj_MSSI, main=paste0("Multiscale straightness index for trajectory: ", select_traj[k])))
     
   }
 }
