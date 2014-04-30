98023d27b9001b1e197e428192d6355d62f8d559
Frank Pennekamp <pennekampster@gmail.com>
2014-04-30 17:46:28 +0200

    Updated vignettes and example data set documented

---
+++ R/MSSI.R
@@ -0,0 +1,14 @@
+#' MSSI.
+#'
+#' @name MSSI
+#' @docType package
+NULL
+
+#' Movement trajectories of aquatic microbes
+#'
+#' @docType data
+#' @keywords datasets
+#' @name example_data
+#' @usage data(example_data)
+#' @format A data frame with example trajectories of aquatic microbes
+NULL
--- inst/doc/MSSI_worker.R
+++ inst/doc/MSSI_worker.R
@@ -3,24 +3,46 @@
 #library(MSSI)
 # getwd()
 #read raw trajectory data, containing unique ID for each trajectory, X- and Y-coordinates and the frame
-# trajectory.data.full <- read.table("/Users/Frank/Documents/Postdoc/Pairwise_species_compare/5 - merged data/MasterData.csv",header=T,row.names=NULL,sep=",",stringsAsFactor=F)
-# trajectory.data <- trajectory.data.full[trajectory.data.full$file == "data00026", ]
-# trajectory.data <- trajectory.data[order(trajectory.data$file,trajectory.data$trajectory,trajectory.data$frame), ]
-# #create unique ID consisting of trajectory ID and file
-# traj <- paste(trajectory.data$file,trajectory.data$trajectory,sep="-")
-# trajectory.data <- cbind(trajectory.data,traj)
-# trajectory.data <- subset(trajectory.data, traj != "Data00026-NA")
-# example_data <- subset(trajectory.data, select=c(traj,frame,X,Y))
-# save(example_data,file="example_data.rda")
+# library(data.table)
+# trajectory.data.full <- fread("/Users/Frank/Documents/Postdoc/Pairwise_species_compare/5 - merged data/MasterData.csv")
+#  trajectory.data <- trajectory.data.full[trajectory.data.full$file == "data00008", ]
+#  trajectory.data <- trajectory.data[order(trajectory.data$file,trajectory.data$trajectory,trajectory.data$frame), ]
+# # #create unique ID consisting of trajectory ID and file
+#  traj <- paste(trajectory.data$file,trajectory.data$trajectory,sep="-")
+#  trajectory.data <- cbind(trajectory.data,traj)
+#  trajectory.data <- subset(trajectory.data, traj != "data00008-NA")
+#  
+
+
+example_data <- as.data.frame(subset(trajectory.data2, select=c(traj,frame,X,Y)))
+example_data <- example_data[,c("traj","frame","X","Y")]
+save(example_data,file="example_data.rda")
 
 data(example_data)
 
+str(example_data)
+rownames(example_data) <- NULL
+
+uniqueID
+
+trajectory.data2 <- as.data.frame(trajectory.data)
 
 # calculate_MSSI function call
-MSSI <- calculate_MSSI(example_data,uniqueID="traj",time="frame",20,20)
+MSSI <- calculate_MSSI(example_data,uniqueID="traj",time="frame",2:5,1)
+
+plot(trajectory.data2$X,trajectory.data2$Y)
 
 # call to plot function
-plot_MSSI(example_data,MSSI,uniqueID="traj",time="frame",granulosity_choosen=10,random=T,N_traj=1)
+plot_MSSI(example_data,MSSI,uniqueID="traj",time="frame",granulosity_choosen=1,random=T,N_traj=2)
 
+example_data$traj <- as.character(example_data$traj)
+
+
+ex <- trajectory.data %.%
+      group_by(traj) %.%
+      summarise(length = length(frame))%.%
+      filter(length>400)
+
+trajectory.data2 <- trajectory.data[trajectory.data$traj %in% ex$traj, ]
 
 
